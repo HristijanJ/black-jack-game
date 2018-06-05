f7eed9515f49c4bfac7cebd84f0c473ef262ac70
HristijanJ <hristijanjovanovski97@gmail.com>
2018-06-05 12:51:39 +0200

    Added Card class

---
+++ BlackJack/BlackJack/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class Card
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+
+        public Card(string val, string type)
+        {
+            Value = val;
+            Type = type;
+        }
+    }
+}
